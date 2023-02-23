615800721d8fd2adef9394de0ff58b205bf05c49
roddazad <roddazad@gmail.com>
2023-02-22 23:18:08 -0800

    trying to post it up heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const db = require('./Develop/db/db.json')
+const uuid = require('./Assets/uuid');
 
 const app = express();
 const port = 3001;
@@ -9,19 +10,23 @@ const port = 3001;
 const PORT = process.env.PORT || 3001;
 
 //GET * should return the index.html file.
-app.use(express.static("public"));
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
+// );
+
+app.use(express.static("Develop/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //GET /notes should return the notes.html file
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/sendFile.html'))
+    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
 });
 
 //GET /api/notes should read the db.json file and return 
 //all saved notes as JSON.
 app.get('/api/notes', (req, res) => {
-    res.json(db)
+    return res.json(db)
 });
 
 //POST /api/notes should receive a new note to save on 
@@ -31,7 +36,32 @@ app.get('/api/notes', (req, res) => {
 //id when it's saved (look into npm packages that could 
 //do this for you).
 app.post('/api/notes', (req, res) => {
+    const {title, text} = req.body;
+        
+        if (title && text){
+            const addedNote = {
+                title,
+                text,
+                note_id: uuid(),
+            };
+            
+            db.push(addedNote)
+            } else {
+                res.json('You must enter a title and a text please');
+            }
+        }
+);
 
-});
+// app.delete('/api/notes/:id', (req, res) => {
+//     const noteId = req.params.id;
+//     if(noteId){
+//         for(let i = 0; i < db.length; i++) {
+//             const currentIdIndex = db[i].id;
+//             if(currentIdIndex === noteId){
+//                 db.pop(db[i])
+//             }
+//         }
+//     }
+// });
 
 app.listen(port, () => console.log(`The server is listening at http://localhost:${PORT}`));

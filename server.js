026b9a5ccd07856160f007eb9c09b4661aa56081
roddazad <roddazad@gmail.com>
2023-02-23 21:49:40 -0800

    last changes

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public/index.html"));
+// });
 
 //GET /api/notes should read the db.json file and return
 //all saved notes as JSON.

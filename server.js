f5c01d8b7acc3a51dd4e4cac60bafc7395b55bde
roddazad <roddazad@gmail.com>
2023-02-23 20:01:20 -0800

    moved public folder out one directory

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const port = 3001;
 
 const PORT = process.env.PORT || 3001;
 
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
-// );
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
-app.use(express.static("Develop/public"));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 // });
 //GET /notes should return the notes.html file
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 //GET /api/notes should read the db.json file and return
